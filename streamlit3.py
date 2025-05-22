a532b90b6925a6ad4d1c10e1cc83932861b16143
KarimLM20 <kbenhafedh@gmail.com>
2025-05-22 21:30:35 +0200

    rajout de planet

--- streamlit3.py
+++ streamlit3.py
@@ -18,22 +18,46 @@ elif selection == "Photos":
 import streamlit as st
 
 # Création de 3 colonnes 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns(3)
 
 # Contenu de la première colonne : 
+
 with col1:
+    st.header("Planet mercury")
+    st.image("https://wallpapercave.com/wp/wp4047226.jpg")
+
+with col2:
+    st.header("Planet Venus")
+    st.image("https://www.lecosmographe.com/wp-content/uploads/2021/01/Venus.jpg")
+
+with col3:
   st.header("Planet earth")
   st.image("https://images.sudouest.fr/2015/07/22/57eb956066a4bd7760c39754/default/1000/la-nasa-publie-sa-premiere-photo-de-la-terre-vue-de-l-espace-depuis-1972.jpg")
 
-# Contenu de la deuxième colonne :
-with col2:
+with col4:
   st.header("Planet Mars")
   st.image("https://wiki.fed-space.com/images/thumb/1/1b/Mars.jpg/1200px-Mars.jpg")
 
-# Contenu de la troisième colonne : 
-with col3:
+with col5:
+  st.header("Planet Jupiter")
+  st.image("https://static.vecteezy.com/system/resources/thumbnails/021/184/177/original/jupiter-planet-3d-footage-space-exploration-video.jpg") 
+
+with col6:
   st.header("Planet Saturn")
   st.image("https://starwalk.space/gallery/images/saturn-planet-guide/1920x1080.jpg")
+
+with col7:
+    st.header("Planet Uranus")
+    st.image("https://wallpaperaccess.com/full/415311.jpg")
+
+with col8:
+    st.header("Planet Neptune")
+    st.image("https://millionfacts.co.uk/wp-content/uploads/2023/02/Interesting-facts-about-Neptune.jpg")
+
+with col9:
+    st.header("Planet Pluto")
+    st.image("https://desetoilespourtous.com/wp-content/uploads/2022/06/Pluton.png")
+
 
   # Création de deux colonnes où col2 est deux fois plus large que col1 :
 col1, col2, col3 = st.columns([2, 5])
